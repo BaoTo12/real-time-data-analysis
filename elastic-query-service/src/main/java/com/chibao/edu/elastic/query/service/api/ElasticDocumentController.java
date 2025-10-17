3767b3b3eb8b1a166f57592eca8db02c5abe4751
ChiBao <baotochi87@gmail.com>
2025-10-17 08:01:18 +0700

    feat(elastic-to-query): add global exception handlers

--- elastic-query-service/src/main/java/com/chibao/edu/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/chibao/edu/elastic/query/service/api/ElasticDocumentController.java
@@ -3,12 +3,12 @@ package com.chibao.edu.elastic.query.service.api;
 import com.chibao.edu.elastic.query.service.business.ElasticQueryService;
 import com.chibao.edu.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.chibao.edu.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -19,25 +19,23 @@ public class ElasticDocumentController {
     private final ElasticQueryService elasticQueryService;
 
     @GetMapping("/")
-    public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
+    public ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
         List<ElasticQueryServiceResponseModel> responseModels = elasticQueryService.getAllDocuments();
         log.info("Elasticsearch returned {} of documents", responseModels);
         return ResponseEntity.ok(responseModels);
     }
 
     @GetMapping("/{id}")
-    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable String id){
+    public ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id) {
         ElasticQueryServiceResponseModel responseModel = elasticQueryService.getDocumentById(id);
         log.debug("Elasticsearch returned document with id {}", id);
         return ResponseEntity.ok(responseModel);
     }
 
     @PostMapping("/get-document-by-text")
-    public @ResponseBody
-    ResponseEntity<List<ElasticQueryServiceResponseModel>>
-    getDocumentByText(@RequestBody ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
+    public ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
         List<ElasticQueryServiceResponseModel> response = elasticQueryService.getDocumentByText(elasticQueryServiceRequestModel.getText());
-        log.info("Elasticsearch returned {} of documents ",response.size());
+        log.info("Elasticsearch returned {} of documents ", response.size());
         return ResponseEntity.ok(response);
     }
 
